a8f7f37c81ae1f63b868f3fc18e204ebeb49bc92
mackenziesavoy <msavoy@unb.ca>
2020-03-11 19:16:04 -0300

    PID MATLAB CODE

---
+++ discrete_pid_coderino_simu_ert_rtw/html/traceInfo_flag.js
@@ -0,0 +1,38 @@
+function TraceInfoFlag() {
+    this.traceFlag = new Array();
+    this.traceFlag["discrete_pid_coderino_simu.cpp:85c28"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:88c29"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:91c24"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:94c39"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:97c12"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:123c12"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:126c37"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:126c47"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:126c54"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:126c78"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:127c24"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:130c16"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:133c23"]=1;
+    this.traceFlag["discrete_pid_coderino_simu.cpp:144c39"]=1;
+}
+top.TraceInfoFlag.instance = new TraceInfoFlag();
+function TraceInfoLineFlag() {
+    this.lineTraceFlag = new Array();
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:85"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:88"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:91"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:94"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:97"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:109"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:123"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:126"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:127"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:130"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:132"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:133"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:135"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:138"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:144"]=1;
+    this.lineTraceFlag["discrete_pid_coderino_simu.cpp:147"]=1;
+}
+top.TraceInfoLineFlag.instance = new TraceInfoLineFlag();
